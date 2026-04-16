80b1d932ad893e7eb723322c317854432a0fe2c1
Kuma <kumapauze@gmail.com>
2026-04-16 11:42:47 -0400

    Import Static

--- src/client/room-renderer.ts
+++ src/client/room-renderer.ts
@@ -7,7 +7,7 @@ import { Vector3 } from "./vector3.js";
  * @param initialRoom The initial room to draw
  * @param newCharacters The set of all characters to subscribe to
  */
-export function initializeRoomRenderer(initialRoom : Room, newCharacters: Set<Character>) {
+export function initializeRoomRenderer(initialRoom: Room, newCharacters: Set<Character>) {
 	// Subscribe to changes from characters
 	newCharacters.forEach(character => {
 		characters.add(character);
@@ -20,24 +20,26 @@ export function initializeRoomRenderer(initialRoom : Room, newCharacters: Set<Ch
 	setCurrentRoom(initialRoom);
 }
 
-let currentRoom : Room; // The current room we're renderering
-const characters : Set<Character> = new Set([]); // Set of characters to listen to
+const staticImageElement: HTMLImageElement = document.querySelector('#room-static') as HTMLImageElement;
+let currentRoom: Room; // The current room we're renderering
+const characters: Set<Character> = new Set([]); // Set of characters to listen to
 
 /**
  * Changes the currently rendered room.
  * @param room 
  */
-export function setCurrentRoom(room : Room) {
-	if (currentRoom != undefined)
-	{
+export function setCurrentRoom(room: Room) {
+	if (currentRoom != undefined) {
 		currentRoom.htmlElement.classList.remove('current');
 		currentRoom.backgroundElement.style.display = 'none';
 	}
-	
+
 	currentRoom = room;
 	currentRoom.htmlElement.classList.add('current');
 	currentRoom.backgroundElement.style.display = 'block';
 	redraw();
+
+	staticImageElement.style.display = currentRoom.isPlayerRoom ? 'none' : 'block';
 }
 
 /**
@@ -46,8 +48,8 @@ export function setCurrentRoom(room : Room) {
 function redraw(): void {
 	// Redraw the characters
 	characters.forEach(character => {
-		const isVisible : boolean = character.getCurrentRoom() == currentRoom;
-		const position : Vector3 = currentRoom.getCharacterPosition(character.name);
+		const isVisible: boolean = character.getCurrentRoom() == currentRoom;
+		const position: Vector3 = currentRoom.getCharacterPosition(character.name);
 		character.redraw(isVisible, position);
 	});
 }
